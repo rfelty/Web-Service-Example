ddd92bc8ab5bddbd9cba187e20b222b47b27648d
rfelty <rfelty@gmail.com>
2015-05-04 21:31:51 -0400

    Initial Steps for the Web Service Project, including the Objective C bridge.

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Rick Felty on 5/4/15.
+//  Copyright (c) 2015 Rick Felty. All rights reserved.
+//
+
+#ifndef Web_Service_Example_bridge_h
+#define Web_Service_Example_bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+#endif
