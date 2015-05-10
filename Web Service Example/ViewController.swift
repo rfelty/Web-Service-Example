3cfc46b0505bf20b0ca557517feaac8fbd0a4340
rfelty <rfelty@gmail.com>
2015-05-10 16:44:34 -0400

    Progress Indicator added

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -13,15 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var myNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.myNameLabel.text = ""
+        //instantiate a gray Activity Indicator View
+        var activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        //add the activity to the ViewController's view
+        view.addSubview(activityIndicatorView)
+        //position the Activity Indicator View in the center of the view
+        activityIndicatorView.center = view.center
+        //tell the Activity Indicator View to begin animating
+        activityIndicatorView.startAnimating()
+        
         let manager = AFHTTPRequestOperationManager()
+
         
         manager.GET( "http:/graph.facebook.com/rickfelty",
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-                println("Response: " + responseObject.description)
+                println("Returned: " + responseObject.description)
                 if let myName = responseObject["name"] as? String {
                     self.myNameLabel.text = myName
                 }
+                activityIndicatorView.removeFromSuperview()
             },
             failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
                 println("Error: " + error.localizedDescription)
